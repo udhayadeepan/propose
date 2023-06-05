60af61f3308bfe38498472c09516ea8fd34626a4
UDHAY <51810768+udhayadeepan@users.noreply.github.com>
2023-06-05 14:47:16 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+mess=['will you be my valentine?','please!','Fuck you']
+i=0
+function changemsg(){
+    
+    if(i<mess.length){
+        
+        let ms=document.getElementById("mes")
+        ms.innerHTML=mess[i]
+        i++;
+    }
+    else{
+        document.getElementById('mes').innerHTML="YOU MESSAGE THE DIAMOND"
+        document.getElementById("mes").style.color="red"
+        document.getElementById("mes").style.fontSize="3em"
+        document.getElementById("yes").style.display='none'
+        document.getElementById("no").style.display='none'
+    }
+}
+
+
+function op(){
+    document.body.style.backgroundImage = 'url(bg.gif)';  
+    document.getElementById('mes').innerHTML="LOVE YOU TOO"
+    document.getElementById("mes").style.color="red"
+    document.getElementById("mes").style.fontSize="3em"
+    document.getElementById("yes").style.display='none'
+    document.getElementById("no").style.display='none'
+    setInterval(bg,1000);
+
+}
